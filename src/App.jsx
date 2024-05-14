7e02d5140a0456bb15ba2373e5960774ad9da61b
m4zin <mazin.sherif@gmail.com>
2024-05-14 16:00:09 +0400

    task: boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import CardContainer from './components/CardContainer'
+
+function App() {
+  return (
+    <>
+      <CardContainer></CardContainer>
+    </>
+
+  )
+}
+
+export default App
